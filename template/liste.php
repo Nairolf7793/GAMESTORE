6689b1d8ffa275f0ee5d04406def7e0876a5b351
Florian <nairolf7793@yahoo.com>
2024-09-20 10:51:53 +0200

    feat(liste) : add CSS

--- template/liste.php
+++ template/liste.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once '../configuration/DbConnection.php';
 require_once '../session.php';
  
@@ -24,76 +23,66 @@ require_once '../session.php';
 <body>
 
 <!-- start navbar -->
-<header>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand">
-      <img src="../asset/LOGO.png" alt="Bootstrap" width="100" height="100">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./template/liste.php">Jeux</a>
-        </li>
-        <?php 
-          
-                if (isset($_SESSION["user"])): ?> 
-                <p>Bonjour <?php echo $_SESSION["user"]["firstname"]; ?> </p>
-                <a href="shopping.php">mon panier</a>
-                <a href="form_ajout.php">Ajout jeux</a></li>
-                <a href="logout.php">Déconnexion</a>
-                
-                <?php else: 
-                    ?>
-                <div class="d-flex justify-content-around">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="form_co.php">Se connecter</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="form_inscription.php">S'inscrire</a>
-                    </li>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand">
+                    <img src="../asset/LOGO.png" alt="Bootstrap" width="100" height="100">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./template/liste.php">Jeux</a>
+                        </li>
+                            <?php 
+                            if (isset($_SESSION["user"])): ?> 
+                            <p>Bonjour <?php echo $_SESSION["user"]["firstname"]; ?> </p>
+                            <a href="shopping.php">mon panier</a>
+                            <a href="form_ajout.php">Ajout jeux</a></li>
+                            <a href="logout.php">Déconnexion</a>
+                            <?php else: 
+                            ?>
+                        <div class="d-flex justify-content-around">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="form_co.php">Se connecter</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="form_inscription.php">S'inscrire</a>
+                            </li>
+                        </div>
+                            <?php endif; ?> 
+                    </ul>
                 </div>
+            </div>
+        </nav>
+    </header>
+<!--end header -->
 
-        <?php endif; ?> 
-      </ul>
-    </div>
-  </div>
-</nav>
-</header>
-
-<body>
-
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <?php foreach ($liste_game as $valeur) { ?>
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $valeur['nom'] ?></h5>
-            <p class="card-text"><?php echo $valeur['genre'] ?></p>
-            <p class="card-text"><?php echo $valeur['prix'] ?>€</p>
-            <p class="card-text"><?php echo $valeur['pegi'] ?></p>
-            <a href="detail.php?valeur_id=<?php echo $valeur['id'] ?>" class="card-link">Detail</a>
+<!-- start section card -->
+    <section class="card">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+            <?php foreach ($liste_game as $valeur) { ?>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $valeur['nom'] ?></h5>
+                        <p class="card-text"><?php echo $valeur['genre'] ?></p>
+                        <p class="card-text"><?php echo $valeur['prix'] ?>€</p>
+                        <p class="card-text"><?php echo $valeur['pegi'] ?></p>
+                        <a href="detail.php?valeur_id=<?php echo $valeur['id'] ?>" class="card-link">Detail</a>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
         </div>
-    </div>
-  </div>
-  <?php } ?>
-</div>
-
-
-
-
-
-
-
-
-   
-    
+    </section>
+<!--end section card --> 
 </body>
 </html>
